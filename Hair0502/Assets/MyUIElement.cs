44b48b11a83f56a2ef58272468a50a7f823bbbb1
sweetiecat <72684148+sweetiecat@users.noreply.github.com>
2021-06-15 14:27:39 +0800

    0615

---
+++ Hair0502/Assets/MyUIElement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+
+
+public class MyUIElement : UIElement
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void OnHandHoverBegin(Hand hand)
+    {
+        base.OnHandHoverBegin(hand);
+        Debug.Log("Hover Begin");
+    }
+
+    protected override void OnHandHoverEnd(Hand hand)
+    {
+        base.OnHandHoverEnd(hand);
+        Debug.Log("Hover End");
+    }
+
+    protected override void HandHoverUpdate(Hand hand)
+    {
+        base.HandHoverUpdate(hand);
+        Debug.Log("Hovering");
+    }
+
+    public event System.Action buttonClick;
+    protected override void OnButtonClick()
+    {
+        base.OnButtonClick();
+
+        Debug.Log("Click");
+        buttonClick.Invoke();
+
+    }
+    private void Start()
+    {
+        buttonClick += OnButtonCilckAdd;
+    }
+
+
+    public void OnButtonCilckAdd()
+    {
+        Debug.Log("Add");
+    }
+}
