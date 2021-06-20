f266b1771a2cb8ed6bbc4741126690d6d2287d6b
iamsherry000 <iamsherry000@gmail.com>
2021-06-21 00:01:49 +0800

    Sherry0621_0001

---
+++ VRHair_UI/Assets/Scripts/ButtonControl/mUIElements.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Valve.VR.InteractionSystem;
+using UnityEngine;
+
+
+public class mUIElements : UIElement
+{
+    public Hand currentHand;
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void OnHandHoverBegin(Hand currentHand)
+    {
+        base.OnHandHoverBegin(currentHand);
+        Debug.Log("Hover Begin");
+    }
+
+    protected override void OnHandHoverEnd(Hand currentHand)
+    {
+        base.OnHandHoverEnd(currentHand);
+        Debug.Log("Hover End");
+    }
+
+    protected override void HandHoverUpdate(Hand currentHand)
+    {
+        base.HandHoverUpdate(currentHand);
+        Debug.Log("Hovering");
+    }
+
+    public event System.Action buttonClick;
+    protected override void OnButtonClick()
+    {
+        base.OnButtonClick();
+
+        Debug.Log("Click");
+        buttonClick.Invoke();
+    }
+    private void Start()
+    {
+        buttonClick += OnButtonCilckAdd;
+    }
+
+
+    public void OnButtonCilckAdd()
+    {
+        Debug.Log("Add");
+    }
+}
